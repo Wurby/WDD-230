d52f638ed559ee1bfb14d5730bf9a064d30813a9
Joshua Pearson <jpearson@ximasoftware.com>
2023-01-09 16:07:15 -0700

    dark mode and mobile menu

--- scripts/portal.js
+++ scripts/portal.js
@@ -18,3 +18,28 @@ anchors.forEach((anchor) => {
     anchor.setAttribute("target", "_blank");
   }
 });
+
+const sunIcon = document.querySelectorAll(".color-mode");
+
+sunIcon.forEach((el) => {
+  el.addEventListener("click", () => {
+    document.querySelectorAll(".color-mode").forEach((item) => {
+      item.classList.toggle("fa-sun");
+      item.classList.toggle("fa-moon");
+    });
+    document.body.classList.toggle("dark");
+    document.querySelector("main").classList.toggle("darkGrey");
+  });
+});
+
+const mobileMenu = document.querySelector("#mobile-menu");
+
+mobileMenu.addEventListener("click", () => {
+  document.querySelector(".mobile-menu-hider").classList.toggle("hidden");
+});
+
+document.body.addEventListener("click", (e) => {
+  if (e.target !== mobileMenu) {
+    document.querySelector(".mobile-menu-hider").classList.toggle("hidden");
+  }
+});
