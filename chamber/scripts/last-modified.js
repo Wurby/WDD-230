f5e0f162ccf319e8b8c54cba912bccc139216f89
Joshua Pearson <jpearson@ximasoftware.com>
2023-01-14 21:58:57 -0700

    Initial Chamber thing

---
+++ chamber/scripts/last-modified.js
@@ -0,0 +1,5 @@
+// A function that returns the last modified date of the webpage
+
+const lastModified = " " + document.lastModified;
+
+document.querySelector("#footer-last-modified").innerHTML += lastModified;
