8bf3f1e11c7578519c4a47105a7f267c67363e3e
Joshua Pearson <jpearson@ximasoftware.com>
2023-01-21 00:31:55 -0700

    Fixed last visted bug and cleaned code

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,8 +1,12 @@
 const visitElement = document.querySelector("#pageVisits");
+const lastVisited = localStorage.getItem("lastVisited");
+const lastVisitedDate = new Date(lastVisited);
 
 const getDaysSinceLastVisit = () => {
-  const lastVisited = localStorage.getItem("lastVisited");
-  const lastVisitedDate = new Date(lastVisited);
+  if (!lastVisited) {
+    localStorage.setItem("lastVisited", new Date().toString());
+    return "First time here!";
+  }
   const now = new Date();
   const diffTime = Math.abs(now - lastVisitedDate);
   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
