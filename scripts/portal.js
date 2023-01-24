525a0f42ae5108d1f5df7fcdb55f05d215295a1c
Joshua Pearson <jpearson@ximasoftware.com>
2023-01-24 13:34:26 -0700

    Team thing

--- scripts/portal.js
+++ scripts/portal.js
@@ -71,3 +71,100 @@ const visitCount = () => {
 };
 
 document.querySelector("#visitCount").innerHTML = visitCount();
+
+const verifyUsername = (str) => {
+  // must be between 1 and 12 characters
+  if (str.length < 5 || str.length > 12) {
+    return false;
+  }
+  // must contain at least one letter and one number
+  const regex = /^(?=.*[a-zA-Z])(?=.*[0-9])/;
+
+  return regex.test(str);
+};
+
+const verifyMatching = (str1, str2) => {
+  return str1 === str2;
+};
+
+const validateForm = () => {
+  const username = document.querySelector("#usernameIdentifier");
+  const formMessage = document.querySelector("#formMessage");
+  const usernameConfirm = document.querySelector("#confirmUsernameIdentifier");
+
+  if (verifyUsername(username.value) === false) {
+    formMessage.innerHTML +=
+      "Username must be between 5 and 12 characters and contain at least one letter and one number. ";
+    formMessage.style.color = "var(--warn)";
+    username.focus();
+    return false;
+  }
+
+  if (verifyMatching(username.value, usernameConfirm.value) === false) {
+    formMessage.innerHTML += "Usernames do not match. ";
+    formMessage.style.color = "var(--warn)";
+    username.focus();
+    return false;
+  }
+
+  return true;
+};
+
+const addTable = (tableData) => {
+  const table = document.querySelector("table");
+  const tableRow1 = document.createElement("tr");
+  const tableRow2 = document.createElement("tr");
+  const tableRow3 = document.createElement("tr");
+  const tableRow4 = document.createElement("tr");
+  const tableRow5 = document.createElement("tr");
+
+  tableRow1.classList.add("th");
+  tableRow1.innerHTML = `
+  <td>Field</td>
+  <td>Value</td>
+  `;
+  tableRow2.innerHTML = `
+  <td>Name</td>
+  <td>${tableData.name}</td>
+  `;
+  tableRow3.innerHTML = `
+  <td>Email</td>
+  <td>${tableData.email}</td>
+  `;
+  tableRow4.innerHTML = `
+  <td>Rating</td>
+  <td>${tableData.rating}</td>
+  `;
+  tableRow5.innerHTML = `
+  <td>Username</td>
+  <td>${tableData.username}</td>
+  `;
+
+  table.appendChild(tableRow1);
+  table.appendChild(tableRow2);
+  table.appendChild(tableRow3);
+  table.appendChild(tableRow4);
+  table.appendChild(tableRow5);
+
+  const tableWrapper = document.querySelector("#table-wrapper");
+
+  const message = document.createElement("p");
+  message.innerHTML =
+    "This does not actually submit the form. It's for demonstration purposes only.";
+  tableWrapper.appendChild(message);
+};
+
+const submitBtn = document.querySelector("#submitBtn");
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (validateForm()) {
+    const tableData = {
+      name: document.querySelector("#fullname").value,
+      email: document.querySelector("#email").value,
+      rating: document.querySelector("#pagerating").value,
+      username: document.querySelector("#usernameIdentifier").value,
+    };
+    addTable(tableData);
+  }
+});
