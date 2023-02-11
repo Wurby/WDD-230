b99bdbbcf89b0c2b701be7d261faf6edd7476dd5
Joshua Pearson <jpearson@ximasoftware.com>
2023-02-10 22:18:45 -0700

    chamber updates

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -4,28 +4,51 @@ const getDirectory = async () => {
   return data;
 };
 
-// grab a random company and set it as the current company
+const filterSilverAndGold = async (companies) => {
+  const silverAndGold = companies.filter((company) => {
+    if (company.membershipLevel === "Silver") {
+      return company;
+    }
+    if (company.membershipLevel === "Gold") {
+      return company;
+    }
+  });
+
+  const randomCompany =
+    silverAndGold[Math.floor(Math.random() * silverAndGold.length)];
+  return randomCompany;
+};
 
 const buildCard = async () => {
   const companies = await getDirectory();
-  const data = companies[Math.floor(Math.random() * companies.length)];
+  const data = filterSilverAndGold(companies);
 
-  console.log(data);
-  const card = document.createElement("div");
-  const image = document.createElement("img");
-  image.src = data.imgUrl;
-  image.alt = data.name;
-  card.appendChild(image);
-  const name = document.createElement("h3");
-  name.textContent = data.name;
-  card.appendChild(name);
-  const website = document.createElement("a");
-  website.href = data.website;
-  website.textContent = data.website;
-  card.appendChild(website);
+  data.then((data) => {
+    const card = document.createElement("div");
+    const image = document.createElement("img");
+    image.src = data.imgUrl;
+    image.alt = data.name;
+    card.appendChild(image);
+    const name = document.createElement("h3");
+    name.textContent = data.name;
+    card.appendChild(name);
+    const website = document.createElement("a");
+    website.href = data.website;
+    website.textContent = data.website;
+    card.appendChild(website);
 
-  const companySpotlight = document.querySelector("#company-spotlight");
-  companySpotlight.appendChild(card);
+    const companySpotlight = document.querySelector("#company-spotlight");
+    companySpotlight.appendChild(card);
+
+    const spotlightTitle = document.querySelector("#spotlightTitle");
+
+    if (data.membershipLevel === "Silver") {
+      spotlightTitle.style.color = "#C0C0C0";
+    }
+    if (data.membershipLevel === "Gold") {
+      spotlightTitle.style.color = "#FFD700";
+    }
+  });
 };
 
 buildCard();
--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,16 +1,15 @@
-const currentTemp = document.querySelector("#current-temp");
-const weatherIcon = document.querySelector("#weather-icon");
-const captionDesc = document.querySelector("figcaption");
-
 const key = "e8d95775b85ae4123aa15bbfb0ae42d1";
 const locationQuery = "lat=41.836594&lon=-111.831303";
 
 const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?${locationQuery}&units=imperial&appid=${key}`;
 
-const apiFetch = async () => {
+const fetchCurrentWeather = async () => {
   try {
     const response = await fetch(weatherUrl);
     if (response.ok) {
+      const currentTemp = document.querySelector("#current-temp");
+      const weatherIcon = document.querySelector("#weather-icon");
+      const captionDesc = document.querySelector("figcaption");
       const data = await response.json();
       console.log(data);
       currentTemp.innerHTML = data.main.temp;
@@ -27,4 +26,56 @@ const apiFetch = async () => {
   }
 };
 
-apiFetch();
+const threeDayForecastUrl = `https://api.openweathermap.org/data/2.5/forecast?${locationQuery}&units=imperial&appid=${key}`;
+
+const fetchThreeDayForecast = async () => {
+  try {
+    const response = await fetch(threeDayForecastUrl);
+    const data = await response.json();
+    const threeDayForecast = data.list
+      .filter((item) => {
+        return item.dt_txt.includes("18:00:00");
+      })
+      .splice(0, 3);
+
+    return threeDayForecast;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+const buildForecast = (data) => {
+  const dayOne = {
+    "day-one": document.querySelector("#day-one"),
+    "day-one-temp": document.querySelector("#day-one-temp"),
+    "day-one-date": document.querySelector("#day-one-date"),
+  };
+  const dayTwo = {
+    "day-two": document.querySelector("#day-two"),
+    "day-two-temp": document.querySelector("#day-two-temp"),
+    "day-two-date": document.querySelector("#day-two-date"),
+  };
+  const dayThree = {
+    "day-three": document.querySelector("#day-three"),
+    "day-three-temp": document.querySelector("#day-three-temp"),
+    "day-three-date": document.querySelector("#day-three-date"),
+  };
+
+  dayOne["day-one-date"].innerHTML = data[0].dt_txt.slice(5, 10);
+  dayOne["day-one-temp"].innerHTML = Math.round(data[0].main.temp);
+  dayOne["day-one-temp"].innerHTML += "°f";
+
+  dayTwo["day-two-date"].innerHTML = data[1].dt_txt.slice(5, 10);
+  dayTwo["day-two-temp"].innerHTML = Math.round(data[1].main.temp);
+  dayTwo["day-two-temp"].innerHTML += "°f";
+
+  dayThree["day-three-date"].innerHTML = data[2].dt_txt.slice(5, 10);
+  dayThree["day-three-temp"].innerHTML = Math.round(data[2].main.temp);
+  dayThree["day-three-temp"].innerHTML += "°f";
+};
+
+fetchCurrentWeather();
+fetchThreeDayForecast().then((data) => {
+  buildForecast(data);
+});
